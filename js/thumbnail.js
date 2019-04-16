bdfe79d87b237c73a83db66a42bdfb9325da52f7
Kekwel <emilien.casano@gmail.com>
2019-04-17 01:24:56 +0200

    add listener + css correction + path name correction
    listener for showing characters color, for setting the character image preview
    minor css correction
    some path were wrong (ics, ptrainer, pacman, dedede)

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -2,19 +2,59 @@ window.onload = function () {
     // TODO init grid perso ?
 }
 
-document.getElementById("j1").oninput = function () {
-    changeName(this.value, "name-j1");
-};
-document.getElementById("j2").oninput = function () {
-    changeName(this.value, "name-j2");
-};
-document.getElementById("j1").onclick = function () {
-    this.setSelectionRange(0, this.value.length)
-};
-document.getElementById("j2").onclick = function () {
-    this.setSelectionRange(0, this.value.length)
-};
+function init(player) {
+    document.getElementById(player).oninput = function () {
+        changeName(this.value, "name-" + player);
+    };
+    document.getElementById(player).onclick = function () {
+        this.setSelectionRange(0, this.value.length)
+    };
+
+    // listener sur stock icons
+    var characters = document.getElementsByClassName('stocks ' + player);
+    for (var i = 0; i < characters.length; i++)(function (i) {
+        characters[i].onclick = function () {
+            setStockColor(player, characters[i].id);
+        };
+    })(i);
+
+    // ninja JS pour garder la valeur de i
+    for (var i = 0; i < 8; i++)(function (i) {
+        var divColor = document.querySelector('#' + player + '-0' + i);
+        divColor.onclick = function () {
+            setImgChar(player, '0' + i, divColor.dataset.char);
+        };
+    })(i);
+}
+
+init("j1");
+init("j2");
 
 function changeName(tag, inputId) {
     document.getElementById(inputId).innerHTML = tag;
 }
+
+function setStockColor(player, name) {
+    for (var i = 0; i < 8; i++)(function (i) {
+        var divColor = document.querySelector('#' + player + '-0' + i);
+        if (name.includes('mii')) {
+            // TODO
+            divColor.src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D';
+        } else {
+            divColor.dataset.char = name;
+            divColor.src = 'img/char/' + name + '/stock_' + name + '_0' + i + '.png';
+        }
+    })(i);
+}
+
+function setImgChar(player, idColor, name) {
+    var stockName = name + '_' + idColor;
+    document.getElementById("char-" + player).src = 'img/char/' + name + '/' + stockName + '.png';
+}
+
+//Et quand init devient trop grosse, tu peux faire un : 
+//function init(name){
+//  nom(name);
+//  couleur(name);
+//  autre(name);
+//}
